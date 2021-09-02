43441c27cfb355ce42f52f8d273a973d12642af6
deletexz <3242449958@qq.com>
2021-09-02 17:17:09 +0800

    Initial commit

--- Photocrawler.py
+++ Photocrawler.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"}
     for i in range(0,1):
         req1 = requests.get(url, headers=headers)
+
         try:
             f = open(str(i)+".jpg", 'wb')
             f.write(req1.content)
